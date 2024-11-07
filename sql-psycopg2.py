f52665be748b80ff011f3b083387195e6807fa91
Matthew Dixon <m.dixon5@outlook.com>
2024-11-07 14:09:29 +0000

    Add basic python fucntions to sql-psycopg2.py folder

---
+++ sql-psycopg2.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+
+
+#connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+#build a cursor object of the database
+cursor = connection.cursor()
+
+
+
+#fetch the results (multiple)
+results = cursor.fetchall()
+
+#fetch the results (single)
+# results = cursor.fetchone()
+
+#close the connection
+connection.close()
+
+#print results
+for result in results:
+    print(result)
