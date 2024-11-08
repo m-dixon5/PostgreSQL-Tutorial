7f295735a17e7b5428c99b98bda966bbfa1b6126
Matthew Dixon <m.dixon5@outlook.com>
2024-11-08 16:28:14 +0000

    Create sql-orm.py file and add imports

--- sql-expression.py
+++ sql-expression.py
@@ -46,16 +46,16 @@ with db.connect() as connection:
     # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
 
     #Query 3 - select only "Queen" from the "Artist" table
-    select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
 
     #Query 4 - select only by "ArtistId" #51 from the "Artist" table
-    select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
 
     #Query 5 - select only the albums with "ArtistId" 51 on the album table
-    select_query = album_table.select().where(album_table.c.ArtistId == 51)
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
 
     #Query 6
-    select_query = track_table.select().where(track_table.c.Composer == "Queen")
+    # select_query = track_table.select().where(track_table.c.Composer == "Queen")
 
 
 
