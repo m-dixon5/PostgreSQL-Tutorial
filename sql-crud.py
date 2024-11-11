e4e6304d716ccb89ea171b28943e042abc73941d
Matthew Dixon <m.dixon5@outlook.com>
2024-11-11 15:49:18 +0000

    Add class for table schema

--- sql-crud.py
+++ sql-crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy import (
-    create_engine, Column, Float, ForeignKey, Integer, String
+    create_engine, Column, Integer, String
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -7,6 +7,18 @@ from sqlalchemy.orm import sessionmaker
 #executing the instructions from the "chinook" database
 db = create_engine("postgresql:///chinook")
 base = declarative_base()
+
+
+#create a class-based table for the "Programmer" table
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(string)
+    last_name  = Column(string)
+    gender = Column(string)
+    nationality = Column(string)
+    famous_for = Column(string)
+
 
 
 # instead of connecting to the database directly, we will ask for a session
