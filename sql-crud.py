679396cf3e6344d87b84d9d52cb5ea44cb8c47c5
Matthew Dixon <m.dixon5@outlook.com>
2024-11-11 16:28:34 +0000

    Add delete functionality for a single record

--- sql-crud.py
+++ sql-crud.py
@@ -109,15 +109,34 @@ first_name = "Matt",
 # session.commit()
 
 #update multiple records
-people = session.query(Programmer)
-for person in people:
-    if person.gender == "F":
-        person.gender = "Female"
-    elif person.gender == "M":
-        person.gender = "Male"
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+
+#deleting a single record
+fname = input("Enter a first name: ")
+lname = input("Enter a last name: ")
+programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+#defensive programming
+if programmer is not None:
+    print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+    confirmation = input("Are you sure you want to delete this record? (y/n) ")
+    if confirmation.lower() == "y":
+        session.delete(programmer)
+        session.commit()
+        print("Programmer deleted")
     else:
-        print("Gender not defined")
-    session.commit()
+        print("Programmer not deleted")
+else:
+    print("No records found")
+
 
 #query the database to find all Programmers
 programmers = session.query(Programmer)
